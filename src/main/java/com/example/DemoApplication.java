fd98b703786bdc3b86c730c720f3722b4baf3686
apotatostory <apotatostory@gmail.com>
2018-01-18 01:59:28 +0800

    自動打包JS + springboot 基礎server

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,24 +1,1 @@
-package com.example;
-
-import java.util.logging.Logger;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.factory.RepositoryFactory;
-
-@SpringBootApplication
-public class DemoApplication {
-	static Logger logger = Logger.getLogger(DemoApplication.class.getName());
-	@Autowired
-	RepositoryFactory repositoryFactory;
-
-	public static void main(String[] args) {
-		logger.info("=======DemoApplication start!!!!");
-		
-		SpringApplication.run(DemoApplication.class, args);
-		
-		logger.info("=======DemoApplication end!!!!");
-	}
-}
+package com.example;import java.util.logging.Logger;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import com.example.factory.RepositoryFactory;@SpringBootApplicationpublic class DemoApplication {	static Logger logger = Logger.getLogger(DemoApplication.class.getName());	@Autowired	RepositoryFactory repositoryFactory;	public static void main(String[] args) {		logger.info("=======DemoApplication start!!!!");		SpringApplication.run(DemoApplication.class, args);		logger.info("=======DemoApplication end!!!!");	}}
